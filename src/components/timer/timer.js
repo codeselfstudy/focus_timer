6f1309745fe2fbcdcaf8cc38e393969af5cc6d3f
J. Cohen <hostelmarketing@gmail.com>
2020-03-30 13:11:07 -0700

    Add timer component

---
+++ src/components/timer/timer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./timer.scss";
+
+const Timer = () => {
+    return (
+        <div className="timer">
+            <div className="time">25:00</div>
+        </div>
+    );
+};
+
+export default Timer;
