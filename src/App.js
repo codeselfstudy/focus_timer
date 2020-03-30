2372b9b73c15cf963e589c57a5ada7b34476ae74
J. Cohen <hostelmarketing@gmail.com>
2020-03-30 13:15:59 -0700

    Add some router code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 import settings from "./settings";
 import Navbar from "./components/navbar/navbar";
 
 import "./App.css";
 
-function App() {
+function App({ currentUser }) {
     return (
         <div className="App">
             <Navbar siteName={settings.site.title} />
+
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route path="/timer" component={TimerPage} />
+                <Route
+                    path="/auth"
+                    render={() =>
+                        currentUser ? <Redirect to="/" /> : <AuthPage />
+                    }
+                />
+            </Switch>
         </div>
     );
 }
