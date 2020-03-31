fefc995bb2c6f9e97c154be67f44f5274060a818
J. Cohen <hostelmarketing@gmail.com>
2020-03-31 11:49:49 -0700

    Begin to add currentUser to Navbar.

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,19 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import { selectCurrentUser } from "../../redux/user/user.selectors";
+
+import { auth } from "../../firebase/firebase.utils";
 
 import "./navbar.scss";
 
-const Navbar = ({ siteName }) => (
-    <nav className="navbar">
-        <div className="left">
-            <span className="site-name">
-                <Link to="/">{siteName}</Link>
-            </span>{" "}
-        </div>
+const Navbar = ({ siteName, currentUser }) => {
+    console.log("[[navbar.js]] currentUser", currentUser);
+    return (
+        <nav className="navbar">
+            <div className="left">
+                <span className="site-name">
+                    <Link to="/">{siteName}</Link>
+                </span>{" "}
+            </div>
 
-        <div className="right">
-            <Link to="/auth">Sign In</Link>
-        </div>
-    </nav>
-);
-export default Navbar;
+            <div className="right">
+                {currentUser ? (
+                    currentUser.displayName
+                ) : (
+                    <Link to="/auth">Sign In</Link>
+                )}
+            </div>
+        </nav>
+    );
+};
+
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+});
+
+export default connect(mapStateToProps)(Navbar);
